e97f7f9ebc5c7c7010c6f60fed6af1a10132128d
Mustafa-Eldeeb <darshm466@gmail.com>
2020-11-21 19:48:23 +0200

    TrafficLight class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,7 +9,7 @@
 
 enum TrafficLightPhase 
 {
-    RED=0,GREEN,YELLOW
+    red=0,green,yellow
 };
 // forward declarations to avoid include cycle
 class Vehicle;
@@ -39,8 +39,10 @@ class TrafficLight :TrafficObject
 {
 public:
     // constructor / desctructor
-
+    TrafficLight();
+    ~TrafficLight();
     // getters / setters
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
     void waitForGreen();
@@ -48,13 +50,14 @@ public:
 
 private:
     // typical behaviour methods
-
+    void cycleThroughPhases();
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
 };
 
 #endif
